a1a2e80d543ff6c493c472cc99619f4d6471c8f7
leozinho <leonardoschwedler@gmail.com>
2025-04-02 09:06:28 +0200

    feat: add SplashEvent and AlertDialog
    - Adds `SplashEvent` for handling user authentication events.
    - Displays an alert dialog when the session has expired, prompting the user to log in again and providing an option to close the app.

--- app/src/main/java/com/example/droidchat/features/splash/presentation/ui/SplashScreen.kt
+++ app/src/main/java/com/example/droidchat/features/splash/presentation/ui/SplashScreen.kt
@@ -16,27 +16,64 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.LifecycleStartEffect
 import com.example.droidchat.R
+import com.example.droidchat.commom.components.AlertDialogCustom
 import com.example.droidchat.commom.theme.BackgroundGradient
 import com.example.droidchat.commom.theme.DroidChatTheme
 import com.example.droidchat.commom.theme.Turquoise80
-import kotlinx.coroutines.delay
+import com.example.droidchat.features.splash.presentation.event.SplashEvent
+import com.example.droidchat.features.splash.presentation.viewmodel.SplashViewModel
 
 @Composable
 fun SplashScreen(
-    navigateToSign: () -> Unit,
+    viewmodel: SplashViewModel = hiltViewModel(),
+    navigateToHome: () -> Unit,
+    navigateToSignIn: () -> Unit,
+    onCloseApp: () -> Unit
 ) {
-    SplashContent()
-    LaunchedEffect(Unit) {
-        delay(3000)
-        navigateToSign()
+
+    val uiState by viewmodel.showErrorDialog.collectAsState()
+
+    LifecycleStartEffect(Unit) {
+        viewmodel.checkSession()
+        onStopOrDispose { }
     }
+
+    LaunchedEffect(Unit) {
+        viewmodel.splashEvent.collect { event ->
+            when (event) {
+                SplashEvent.UserAuthenticated -> {
+                     navigateToHome()
+                }
+                SplashEvent.UserNotAuthenticated -> {
+                    navigateToSignIn()
+                }
+            }
+        }
+    }
+
+    if (uiState){
+        AlertDialogCustom(
+            onDismissRequest =  onCloseApp ,
+            onConfirmButton =  onCloseApp ,
+            confirmButtonText = stringResource(R.string.fechar_app),
+            text = stringResource(R.string.sess_o_expirada_fa_a_login_novamente)
+        )
+    }
+
+    SplashContent()
+
 }
 
 @Composable
